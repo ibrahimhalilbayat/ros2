7ef1e8541b5e1d95ccb25df4c44ff34349f22fc6
İbrahim Halil Bayat <61160630+ibrahimhalilbayat@users.noreply.github.com>
2024-05-21 10:57:18 +0300

    Create launch_file.launch.py

---
+++ src/launch_package/launch/launch_file.launch.py
@@ -0,0 +1,62 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    le_launch_description = LaunchDescription()
+
+    isengard_node = Node(
+        package="python_package",
+        executable="isengard_node",
+        parameters=[
+            {"publishing_frequency": 2.0}
+        ]
+    )
+
+    minas_tirith_node = Node(
+        package="python_package",
+        executable="minas_tirith_node",
+        parameters=[
+            {"publishing_frequency": 2.0}
+        ]
+    )
+
+    mordor_node = Node(
+        package="python_package",
+        executable="mordor_node",
+        parameters=[
+            {"publishing_frequency": 2.0}
+        ]
+    )
+
+    moria_node = Node(
+        package="python_package",
+        executable="moria_node",
+        parameters=[
+            {"publishing_frequency": 2.0}
+        ]
+    )
+
+    riven_dell_node = Node(
+        package="python_package",
+        executable="riven_dell_node",
+        parameters=[
+            {"publishing_frequency": 2.0}
+        ]
+    )
+
+    palantir_node = Node(
+        package="python_package",
+        executable="palantir_node",
+        parameters=[
+            {"publishing_frequency": 2.0}
+        ]
+    )
+
+    le_launch_description.add_action(isengard_node)
+    le_launch_description.add_action(mordor_node)
+    le_launch_description.add_action(riven_dell_node)
+    le_launch_description.add_action(moria_node)
+    le_launch_description.add_action(minas_tirith_node)
+    le_launch_description.add_action(palantir_node)
+
+    return le_launch_description
