ab04e2cce71ec9747c0ae93ed77c635c4a991979
İbrahim Halil Bayat <61160630+ibrahimhalilbayat@users.noreply.github.com>
2024-05-21 11:31:13 +0300

    Update launch_file.launch.py

--- src/launch_package/launch/launch_file.launch.py
+++ src/launch_package/launch/launch_file.launch.py
@@ -1,59 +1,32 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    le_launch_description = LaunchDescription()
+    launch_description = LaunchDescription()
+    location_names = ["Rivendell", "Moria", "Mordor", "Minastirith", "Isengard"]
+    news_nodes = []
 
-    isengard_node = Node(
-        package="python_package",
-        executable="isengard_node",
-        parameters=[
-            {"publishing_frequency": 2.0}
-        ]
-    )
+    for location in location_names:
+        news_nodes.append(Node(
+            package="python_package",
+            executable="news_of_middle_earth_node",
+            name= "news_from_" + location.lower(),
+            parameters=[
+                {"location_name": location,
+                 "publish_freq": 2.0}          
+            ]
+        ))
 
-    minas_tirith_node = Node(
-        package="python_package",
-        executable="minas_tirith_node",
-        parameters=[
-            {"publishing_frequency": 2.0}
-        ]
-    )
-
-    mordor_node = Node(
-        package="python_package",
-        executable="mordor_node",
-        parameters=[
-            {"publishing_frequency": 2.0}
-        ]
-    )
-
-    moria_node = Node(
-        package="python_package",
-        executable="moria_node",
-        parameters=[
-            {"publishing_frequency": 2.0}
-        ]
-    )
-
-    riven_dell_node = Node(
-        package="python_package",
-        executable="riven_dell_node",
-        parameters=[
-            {"publishing_frequency": 2.0}
-        ]
-    )
 
     palantir_node = Node(
         package="python_package",
-        executable="palantir_node"
+        executable="palantir_node",
+        name="palantir"
     )
 
-    le_launch_description.add_action(isengard_node)
-    le_launch_description.add_action(mordor_node)
-    le_launch_description.add_action(riven_dell_node)
-    le_launch_description.add_action(moria_node)
-    le_launch_description.add_action(minas_tirith_node)
-    le_launch_description.add_action(palantir_node)
+    for node in news_nodes:
+        launch_description.add_action(node)
+    launch_description.add_action(palantir_node)
 
-    return le_launch_description
+    return launch_description
